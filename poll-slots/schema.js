3b2195d7f130b55b7ab0578e46868762ee6cdf59
Mayukh Nair <nairmayukh@gmail.com>
2021-05-02 17:55:02 +0530

    Revised schema

--- poll-slots/schema.js
+++ poll-slots/schema.js
@@ -7,31 +7,52 @@ mongoose.connect(url, { useNewUrlParser: true })
 
 const db = mongoose.connection
 
-const stateSchema = new Schema({
-    id: Number,
-    name: String,
+const dataSchema = new Schema({
     districts: new Schema({
-        id: Number,
-        name: String,
-        centers: new Schema({
+        18: new Schema({
             id: Number,
             name: String,
-            pin: Number,
-            state: String,
-            dist: String,
-            block: String,
-            fee: String,
-            slots: new Schema({
+            state_name: String,
+            state_id = Number,
+            centers: new Schema({
                 id: Number,
-                age_limit: Number,
-                vax: String,
-                timings: [String]
+                name: String,
+                pin: Number,
+                state: String,
+                dist: String,
+                block: String,
+                fee: String,
+                slots: new Schema({
+                    id: Number,
+                    age_limit: Number,
+                    vax: String,
+                    timings: [String]
+                })
+            })
+        }),
+        45: new Schema({
+            id: Number,
+            name: String,
+            centers: new Schema({
+                id: Number,
+                name: String,
+                pin: Number,
+                state: String,
+                dist: String,
+                block: String,
+                fee: String,
+                slots: new Schema({
+                    id: Number,
+                    age_limit: Number,
+                    vax: String,
+                    timings: [String]
+                })
             })
         })
     })
 })
 
-module.exports = mongoose.model('States', stateSchema)
+module.exports = mongoose.model('Data', dataSchema)
 
 // const centerSchema = new Schema({
 //     id: Number,
